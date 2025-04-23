8a06c28ceac9b4fea08ffda62fa42b2f1aa9dfb9
Maria <maria.radmer@modl.ai>
2025-04-23 15:54:48 +0200

    added the gloabe user widget

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Maria Radmer & Leah
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
